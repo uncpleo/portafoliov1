ebbdec89da79ddf01360538641adf308308b5a12
River Leonardo Belito Chahuayllo <143974433+uncpleo@users.noreply.github.com>
2024-05-27 00:01:39 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,7 @@ const Projects = () => {
                         initial={{opacity:0, x:100}}
                         transition={{duration:1}}
                         className="w-full max-w-xl lg:w-3/4">
-                        <h6 className="mb-2 font-semibold">{project.title}</h6>
+                        <a target="_blank" href={project.link}><h6 className="mb-2 font-semibold">{project.title}</h6></a>
                         <p className="mb-4 text-neutral-400">{project.description}</p>
                         {project.technologies.map((tech, index) => (
                             <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-green-700">{tech}</span>
